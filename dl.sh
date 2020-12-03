ff139fe2c76195ca67ab8fdb8570f68b1afe5f91
sj <sebastianthegreatful@gmail.com>
2020-12-03 11:46:49 +0700

    cd into $DOWNLOAD_DIRECTORY before running youtube-dl

--- dl.sh
+++ dl.sh
@@ -3,10 +3,8 @@
 # If $DOWNLOAD_DIRECTORY is empty assign default value "./Downloads" to it
 : "${DOWNLOAD_DIRECTORY:=./Downloads}"
 
-# TODO: cd $DOWNLOAD_DIRECTORY ? And thereby avoid "mv *.mp3 $DOWNLOAD_DIRECTORY/"?
+cd $DOWNLOAD_DIRECTORY || return
 
 # TODO: Add environment variables for most of the below arguments
 # TODO: Add generic environment variables YOUTUBE_DL_OPT
-youtube-dl $YOUTUBE_DL_OPT -i --extract-audio --audio-quality 0 --audio-format mp3 --add-metadata --download-archive $DOWNLOAD_DIRECTORY/.downloaded $1 && rm -f *.m4a
-
-mv *.mp3 $DOWNLOAD_DIRECTORY/
+youtube-dl $YOUTUBE_DL_OPT -i --extract-audio --audio-quality 0 --audio-format mp3 --add-metadata --download-archive ./.downloaded $1 && rm -f *.m4a
