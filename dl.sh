a8f703afd351b68711c0520181387e15c41d0e20
sj <sebastianthegreatful@gmail.com>
2020-12-03 11:48:09 +0700

    Expose additional parameters via environment variables

--- dl.sh
+++ dl.sh
@@ -2,9 +2,9 @@
 
 # If $DOWNLOAD_DIRECTORY is empty assign default value "./Downloads" to it
 : "${DOWNLOAD_DIRECTORY:=./Downloads}"
+: "${AUDIO_QUALITY:=0}"
+: "${AUDIO_FORMAT:=mp3}"
 
 cd $DOWNLOAD_DIRECTORY || return
 
-# TODO: Add environment variables for most of the below arguments
-# TODO: Add generic environment variables YOUTUBE_DL_OPT
-youtube-dl $YOUTUBE_DL_OPT -i --extract-audio --audio-quality 0 --audio-format mp3 --add-metadata --download-archive ./.downloaded $1 && rm -f *.m4a
+youtube-dl $YOUTUBE_DL_OPT -i --extract-audio --audio-quality $AUDIO_QUALITY --audio-format $AUDIO_FORMAT --add-metadata --download-archive ./.downloaded $1 && rm -f *.m4a
